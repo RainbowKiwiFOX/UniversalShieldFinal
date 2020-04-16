740e5e0359b4e37cb1389cf41d90e124f205279d
Victor Nikitchuk <rainbowkiwifox@gmail.com>
2020-04-16 12:34:25 +0300

    Добавлена возможность изменения контура кнопки по нажатию и отпусканию

--- UniversalShieldDrivers/Display/Inc/DisplayButtons.h
+++ UniversalShieldDrivers/Display/Inc/DisplayButtons.h
@@ -11,7 +11,7 @@ enum buttonForm {
 	roundRectangle_f,	//Скруглённый прямоугольник
 	circle_f					//Круг
 };
-//Структура кнопки
+//Структура визуальной кнопки
 typedef struct {
 	uint16_t id;					//Индификатор кнопки. Может быть любое число от 0 до 65535
 	uint16_t posX;				//Позиция кнопки по X. Если это круг, то это X центра
@@ -19,19 +19,22 @@ typedef struct {
 	uint16_t length;			//Длина по X. Если это круг, то используется как радиус
 	uint16_t width;				//Ширина по Y. Если это круг, то не используется
 	//Указатель на функцию, которая будет выполняться при нажатии на кнопку
-	void (*action)(uint16_t id, touchStates ts);	
+	void (*action)(uint16_t id, touchStates ts);
 } button_t;
 //Структура кнопки
 typedef struct {
-	button_t *button;			//Основная кнопка
-	uint8_t form;					//Форма кнопки
-	uint16_t borderСolor;	//Цвет линии кнопки
-	uint8_t borderdWidth;	//Ширина линии кнопки
-	uint16_t fillColor;		//Цвет заливки кнопки
+	button_t *button;						//Основная кнопка
+	uint8_t form;								//Форма кнопки
+	uint16_t borderСolor;				//Цвет контура кнопки
+	uint16_t activeBorderColor;	//Цвет контура при нажатии на неё
+	uint8_t borderdWidth;				//Ширина контура кнопки
+	uint16_t fillColor;					//Цвет заливки кнопки
 } visualButton_t;
 
 void printButtons(visualButton_t *buttons, uint8_t count);
 void buttonsTouchHandler(button_t *buttons, uint8_t count, touch_t t);
 void visualizeButton(button_t button, uint16_t color);
+void printButton(visualButton_t button, uint8_t activeStatus);
+void checkActiveButtons(visualButton_t *buttons, uint8_t count, touch_t t);
 
 #endif
--- UniversalShieldDrivers/Display/Src/DisplayButtons.c
+++ UniversalShieldDrivers/Display/Src/DisplayButtons.c
@@ -1,60 +1,86 @@
 #include "DisplayButtons.h"
-//Функция печати кнопок на экране
+
+//TODO: Норм обработка круглых кнопок
+//TODO: Добавить треугольные кнопки
+//TODO: Кнопки с рисунками
+//TODO: Кнопки с текстом
+//TODO: Кнопки с кастомной отрисовкой по вызову функции
+
+//Функция проверки наличия точки нажатия внутри кнопки
+uint8_t checkPosition(button_t b, touch_t t) {
+			return	 (t.x >= b.posX)  		//Позиция X больше или равна началу кнопки
+			&& (t.x <= b.posX+b.length) 	//Позиция X меньше или равна концу кнопки
+			&& (t.y >= b.posY)  					//Позиция Y больше или равна началу кнопки
+			&& (t.y <= b.posY+b.width);		//Позиция Y меньше или равна концу кнопки
+}
+
+//Функция печати массива кнопок на экране
 void printButtons(visualButton_t *buttons, uint8_t count) {
 	while(count--) {
-		//Вызов функции рисования в зависимости от формы кнопки
-		switch(buttons[count].form) {
-			case rectangle_f: //Отрисовка прямоугольника
-				if((buttons[count].borderdWidth != 0) && (buttons[count].borderСolor != TFT_COLOR_none))
-				TFT_drawRectangle(buttons[count].button->posX, buttons[count].button->posY, 
-													buttons[count].button->length, buttons[count].button->width, 
-													buttons[count].borderdWidth, buttons[count].borderСolor);
-			//Если есть внутренняя заливка, то её тоже нужно отрисовать
-				if(buttons[count].fillColor != TFT_COLOR_none)
-					TFT_fillRectangle(buttons[count].button->posX+buttons[count].borderdWidth, buttons[count].button->posY+buttons[count].borderdWidth, 
-													buttons[count].button->length-2*buttons[count].borderdWidth, buttons[count].button->length-2*buttons[count].borderdWidth, 
-													buttons[count].fillColor);
-				break;
-			case roundRectangle_f:
-				if(buttons[count].borderdWidth != 0)
-				TFT_drawRoundRect(buttons[count].button->posX, buttons[count].button->posY,
-													buttons[count].button->length, buttons[count].button->width,
-													buttons[count].borderdWidth*2.5f, buttons[count].borderdWidth,
-													buttons[count].borderСolor);
-				//Если есть внутренняя заливка, то её тоже нужно отрисовать
-				if(buttons[count].fillColor != TFT_COLOR_none)
-				TFT_fillRoundRect(buttons[count].button->posX+buttons[count].borderdWidth, buttons[count].button->posY+buttons[count].borderdWidth,
-													buttons[count].button->length-2*buttons[count].borderdWidth, buttons[count].button->length-2*buttons[count].borderdWidth,
-													buttons[count].borderdWidth*2.5f, buttons[count].fillColor);
-				break;
-			case circle_f:
-				if(buttons[count].borderdWidth != 0)
-				TFT_drawCircle(buttons[count].button->posX, buttons[count].button->posY,
-													buttons[count].button->length, buttons[count].borderdWidth, 
-													buttons[count].borderСolor);
-				if(buttons[count].fillColor != TFT_COLOR_none)
-				TFT_fillCircle(buttons[count].button->posX, buttons[count].button->posY,
-													buttons[count].button->length-buttons[count].borderdWidth, 
-													buttons[count].fillColor);
-				break;
-		}
+		printButton(buttons[count], 0);
 	}
 }
 
-//TODO: Норм обработка круглых кнопок
+//Функция изменения контура кнопки при нажатии или отпускании
+void checkActiveButtons(visualButton_t *buttons, uint8_t count, touch_t t) {
+	while(count--) {
+		//Если точка внутри кнопки, то рисование контура в зависимости от состояния нажатия
+		if(checkPosition(*buttons[count].button, t)) {
+			if(t.state == T_pressed) printButton(buttons[count], 1); //Рисование активного контура
+			if(t.state == T_released) printButton(buttons[count], 0); //Рисование обычного контура
+		}			
+	}
+}
+
 //Функция обработки нажатий на экран
 void buttonsTouchHandler(button_t *buttons, uint8_t count, touch_t t) {
 	if(t.state == T_noTouch) return;
 	//Циклическая проверка координат нажатия и кнопки	
 	while(count--) {
-		if(
-			   (t.x >= buttons[count].posX)  												//Позиция X больше или равна началу кнопки
-			&& (t.x <= buttons[count].posX+buttons[count].length) 	//Позиция X меньше или равна концу кнопки
-			&& (t.y >= buttons[count].posY)  												//Позиция Y больше или равна началу кнопки
-			&& (t.y <= buttons[count].posY+buttons[count].width)		//Позиция Y меньше или равна концу кнопки
-		) {
+		if(checkPosition(buttons[count], t)) {
 			buttons[count].action(buttons[count].id, t.state); //Вызов функции обработки нажатия на кнопку
 		}
+	}
+}
+
+//Функция печати контура и фона кнопки
+void printButton(visualButton_t button, uint8_t activeStatus) {
+	//Вызов функции рисования в зависимости от формы кнопки
+	switch(button.form) {
+		case rectangle_f: //Отрисовка прямоугольника
+			//Рисование контура
+			if((button.borderdWidth != 0) && (button.borderСolor != TFT_COLOR_none || button.activeBorderColor != TFT_COLOR_none))
+			TFT_drawRectangle(button.button->posX, button.button->posY, 
+												button.button->length, button.button->width, 
+												button.borderdWidth, activeStatus ? button.activeBorderColor : button.borderСolor);
+		//Если есть внутренняя заливка, то её тоже нужно отрисовать
+			if(button.fillColor != TFT_COLOR_none)
+				TFT_fillRectangle(button.button->posX+button.borderdWidth, button.button->posY+button.borderdWidth, 
+												button.button->length-2*button.borderdWidth, button.button->length-2*button.borderdWidth, 
+												button.fillColor);
+			break;
+		case roundRectangle_f:
+			if((button.borderdWidth != 0) && (button.borderСolor != TFT_COLOR_none || button.activeBorderColor != TFT_COLOR_none))
+			TFT_drawRoundRect(button.button->posX, button.button->posY,
+												button.button->length, button.button->width,
+												button.borderdWidth*2.5f, button.borderdWidth,
+												activeStatus ? button.activeBorderColor : button.borderСolor);
+			//Если есть внутренняя заливка, то её тоже нужно отрисовать
+			if(button.fillColor != TFT_COLOR_none)
+			TFT_fillRoundRect(button.button->posX+button.borderdWidth, button.button->posY+button.borderdWidth,
+												button.button->length-2*button.borderdWidth, button.button->length-2*button.borderdWidth,
+												button.borderdWidth*2.5f, button.fillColor);
+			break;
+		case circle_f:
+			if((button.borderdWidth != 0) && (button.borderСolor != TFT_COLOR_none || button.activeBorderColor != TFT_COLOR_none))
+			TFT_drawCircle(button.button->posX, button.button->posY,
+												button.button->length, button.borderdWidth, 
+												activeStatus ? button.activeBorderColor : button.borderСolor);
+			if(button.fillColor != TFT_COLOR_none)
+			TFT_fillCircle(button.button->posX, button.button->posY,
+												button.button->length-button.borderdWidth, 
+												button.fillColor);
+			break;
 	}
 }
 
